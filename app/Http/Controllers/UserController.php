c5c1b3f00ac3fbf827325bd40fa40eaa9d9dd0fd
innotuyi <innotuyi67@gmail.com>
2022-03-16 18:06:13 +0200

    Adding documenation

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -32,6 +32,74 @@ class UserController extends Controller
     }
 
 
+    /**
+     * User Login
+     *
+     * @OA\Post (
+     *     path="/api/login",
+     *     tags={"Users"},
+     *     @OA\RequestBody(
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 @OA\Property(
+     *                      type="array",
+     *                       @OA\Items(
+     *                      @OA\Property(
+     *                          property="email",
+     *                          type="string"
+     *                      ),
+     *                      @OA\Property(
+     *                          property="password",
+     *                          type="string"
+     *                      ),
+     *                     
+     *                     
+     *                     
+     *                     ),
+     *                 ),
+     *                 example={
+     *                     "email":"example@content.com",
+     *                     "password":"ishyamba123",
+     *                     
+     *                }
+     *             )
+     *         )
+     *      ),
+     *      @OA\Response(
+     *          response=201,
+     *          description="successfully Logged In",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="Token", type="string", example="2|aAUDFJ8GbMcvrFH2PnDvDZ2GM8cbklBijDqMX9Dw"),
+     *          )
+     *   
+     *      ),
+     * 
+     *      @OA\Response(
+     *          response=401,
+     *          description="invalid Credentials",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="msg", type="string", example="fail"),
+     *          )
+     *      ),
+ *  @OA\Response(
+ *    response=500,
+ *    description="Returns when there is server problem",
+ *    @OA\JsonContent(
+ *       @OA\Property(property="message", type="string", example="Server Error"),
+ *    )  
+ * ),
+ *  @OA\Response(
+     *          response=419,
+     *          description="CSRF Token mismatch",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="msg", type="string", example="fail"),
+     *          )
+     *      ),   
+     * )
+     */
+
+
     public function login(Request $request) {
         $fields = $request->validate([
             'email' => 'required|string',
@@ -57,6 +125,27 @@ class UserController extends Controller
 
         return response($response, 201);
     }
+
+         /**
+ * @OA\Post(
+ * path="/api/logout",
+ * summary="Logout",
+ * description="Logout user and invalidate token",
+ * tags={"Users"},
+ * security={ {"bearer": {} }},
+ * @OA\Response(
+ *    response=200,
+ *    description="Success"
+ *     ),
+ * @OA\Response(
+ *    response=401,
+ *    description="Returns when user is not authenticated",
+ *    @OA\JsonContent(
+ *       @OA\Property(property="message", type="string", example="Not authorized"),
+ *    )
+ * )
+ * )
+ */
 
     public function logout(Request $request) {
         auth()->user()->tokens()->delete();
