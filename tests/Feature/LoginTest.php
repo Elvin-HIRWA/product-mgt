a07c1465eb6b9a740f094d6c3cb389b9e1d29a79
innotuyi <innotuyi67@gmail.com>
2022-03-17 10:55:36 +0200

    adding login test

---
+++ tests/Feature/LoginTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class LoginTest extends TestCase
+{
+    use DatabaseMigrations;
+    
+    /** @test */
+    public function check_user_login() {
+
+        $user = User::Create([
+            "name" =>"innocent",
+            "email" =>"innocent@gmail.com",
+            "password" =>bcrypt("innocent"),
+        ]); 
+
+     $response = $this->postJson('/api/login', [
+         'email' => $user->email,
+         'password' => "innocent"
+     ]);
+     $response->assertStatus(200);
+     $response->assertJsonStructure([
+         'token',
+     ])->json();
+     
+     $token = $response->json('token');
+    }
+}
