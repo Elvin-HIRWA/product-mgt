0a4b1b561ed0a9bcca26d4e6555c15aa614256b5
innotuyi <innotuyi67@gmail.com>
2022-03-17 09:52:29 +0200

    register test

---
+++ tests/Feature/RegisterTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class RegisterTest extends TestCase
+{use DatabaseMigrations;
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    public function testUserRegisterSuccessfully() {
+
+        $UserData = [
+            "name"=>"innocent",
+            "email"=>"innocent@gmail.com",
+            "password"=>"innocent",
+            "password_confirmation"=>"innocent"
+        ];   
+        
+        $response = $this->postJson('/api/register', $UserData);
+        $response->assertStatus(200);  
+        $response->assertExactJson([
+            "name"=>"innocent",
+            "email"=>"innocent@gmail.com"
+            
+        ]); 
+    }
+}
