05981cd5793d85f4bc25eb8943ebb9b72a4fba12
innotuyi <innotuyi67@gmail.com>
2022-03-24 11:47:16 +0200

    Adding some changes

--- tests/Feature/ProductsTest.php
+++ tests/Feature/ProductsTest.php
@@ -35,15 +35,15 @@ class ProductsTest extends TestCase
         $response = $this->actingAs($user)->postJson("/api/product", [
             "name" => "innoss",
             "description" => "Kimihurora mu rwanda",
-            "price" => "30",
-            "quantity" => "5",
+            "price" => 30,
+            "quantity" => 5,
         ]);
         $response->assertStatus(201);
         $response->assertExactJson([       
                 "name" => "innoss",
                 "description" => "Kimihurora mu rwanda",
-                "price" => "30",
-                "quantity" => "5",
+                "price" => 30,
+                "quantity" => 5,
             ]);
         $this->assertCount(1,Product::all());
     }
@@ -76,45 +76,45 @@ class ProductsTest extends TestCase
     }
 
     // /** @test */
-    // public function a_user_can_read_all_the_product()
-    // {
-    //     $user = User::create([
-    //         "name" => "innoss",
-    //         "email" => "Kimihurora@murwanda.com",
-    //         "password" => "elvin30",
-    //         "password_confirmation" => "elvin30"
-    //     ]);
-    //     Product::create([
-    //         "name" => "innoss",
-    //         "description" => "Kimihurora mu rwanda",
-    //         "price" => 30,
-    //         "quantity" => 5,
-    //     ]);
+    public function a_user_can_read_all_the_product()
+    {
+        $user = User::create([
+            "name" => "innoss",
+            "email" => "Kimihurora@murwanda.com",
+            "password" => "elvin30",
+            "password_confirmation" => "elvin30"
+        ]);
+        Product::create([
+            "name" => "innoss",
+            "description" => "Kimihurora mu rwanda",
+            "price" => 30,
+            "quantity" => 5,
+        ]);
 
-    //     // Product::create([
-    //     //     "name" => "innoss",
-    //     //     "description" => "Kimihurora mu rwanda",
-    //     //     "price" => "30",
-    //     //     "quantity" => "5",
-    //     // ]);
+        // Product::create([
+        //     "name" => "innoss",
+        //     "description" => "Kimihurora mu rwanda",
+        //     "price" => "30",
+        //     "quantity" => "5",
+        // ]);
 
-    //     $response = $this->actingAs($user)->getJson('/api/product');
-    //     $response->assertStatus(200);
-    //     $response->assertExactJson([
-    //         [
-    //             "name" => "innoss",
-    //             "description" => "Kimihurora mu rwanda",
-    //             "price" => 30,
-    //             "quantity" => 5,
-    //         ],
-    //         // [
-    //         //     "name" => "innoss",
-    //         //     "description" => "Kimihurora mu rwanda",
-    //         //     "price" => 30,
-    //         //     "quantity" => 5,
-    //         // ]
-    //     ]);
-    // }
+        $response = $this->actingAs($user)->getJson('/api/product');
+        $response->assertStatus(200);
+        $response->assertExactJson([
+            [
+                "name" => "innoss",
+                "description" => "Kimihurora mu rwanda",
+                "price" => 30,
+                "quantity" => 5,
+            ],
+            // [
+            //     "name" => "innoss",
+            //     "description" => "Kimihurora mu rwanda",
+            //     "price" => 30,
+            //     "quantity" => 5,
+            // ]
+        ]);
+    }
 
     /** @test */
     public function can_update_a_product()
@@ -128,22 +128,22 @@ class ProductsTest extends TestCase
        $product = Product::create([
             "name" => "innoss",
             "description" => "Kimihurora mu rwanda",
-            "price" => "30",
-            "quantity" => "5",
+            "price" => 30,
+            "quantity" => 5,
         ]);
 
         $response = $this->actingAs($user)->putJson("/api/product/{$product->id}", [
             "name" => "Ndoli",
             "description" => "Kimihurora mu rwanda",
-            "price" => "30",
-            "quantity" => "5",
+            "price" => 30,
+            "quantity" => 5,
         ]);
         $response->assertStatus(200);
         $response->assertExactJson([
             "name" => "Ndoli",
             "description" => "Kimihurora mu rwanda",
-            "price" => "30",
-            "quantity" => "5",
+            "price" => 30,
+            "quantity" => 5,
         ]);
     }
     /** @test */
@@ -170,29 +170,30 @@ class ProductsTest extends TestCase
     }
 
     /** @test */
-    // public function can_search_a_product_with_related_name()
-    // {
-    //     $user = User::create([
-    //         "name" => "Elvin",
-    //         "email" => "elhirwa3@gmail.com",
-    //         "password" => "landlord",
-    //         "password_confirmation" => "landlord"
-    //     ]);
-    //     $product = Product::create([
+    public function can_search_a_product_with_related_name()
+    {
+        $user = User::create([
+            "name" => "Elvin",
+            "email" => "elhirwa3@gmail.com",
+            "password" => "landlord",
+            "password_confirmation" => "landlord"
+        ]);
+        $product = Product::create([
         
-    //         "name" => "innoss",
-    //         "description" => "Kimihurora mu rwanda",
-    //         "price" => 30,
-    //         "quantity" => 5
-    //     ]);
-    //     $response = $this->actingAs($user)->getJson("/api/product/search/{$product->name}");
-    //     $response->assertStatus(200);
-    //     $response->assertExactJson([
-    //         "name" => "innoss",
-    //         "description" => "Kimihurora mu rwanda",
-    //         "price" =>30,
-    //         "quantity" =>5
-    //     ]);
-    // }
+            "name" => "innoss",
+            "description" => "Kimihurora mu rwanda",
+            "price" => 30,
+            "quantity" => 5
+        ]);
+        $response = $this->actingAs($user)->getJson("/api/product/search/{$product->name}");
+        $response->assertStatus(200);
+        // $response->dd();
+        $response->assertExactJson([
+          [  "name" => "innoss",
+            "description" => "Kimihurora mu rwanda",
+            "price" =>30,
+            "quantity" =>5]
+        ]);
+    }
     
 }
